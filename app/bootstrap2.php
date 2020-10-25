aa7255ec7df602ec1c90ad9071579efaa3aac26a
Eduardo Luz <eduluz1976@gmail.com>
2020-10-25 00:02:07 -0700

    Cleanup code and adding ViewUtils filters

--- app/bootstrap2.php
+++ app/bootstrap2.php
@@ -7,6 +7,7 @@ use Phalcon\Di\FactoryDefault;
 use Phalcon\Http\ResponseInterface;
 use Dotenv\Dotenv;
 use \Harpya\SDK\Constants;
+use \Harpya\SDK\Core\ViewUtils;
 use Phalcon\Mvc\Micro;
 
 try {
@@ -40,10 +41,11 @@ try {
         $di->register(new $provider());
     }
 
-    // if ($app->request->getHeader(''))
-
     $app = \Harpya\IP\Application::getInstance($di);
     $app->loadRoutesFromFolder($rootPath . '/routes');
+
+    ViewUtils::initFilters();
+    ViewUtils::addFilters($di);
 
     // X-Skip-Session
     if (!$app->request->getHeader('X-Skip-Session')) {
@@ -67,109 +69,9 @@ try {
 
     $localAuthData = $app->session->get('auth_data');
 
-    // $router = $di['router'];
-
-    // $handle = $router->handle(
-    //     $_SERVER['REQUEST_URI']
-    // );
-
-    // $view = $di['view'];
-
-    // View
-    // $view->start();
-
-    // $dispatcher = $di['dispatcher'];
-
-    // $controllerName = $router->getControllerName() ?? 'index';
-
-    // $dispatcher->setControllerName(
-    //     $controllerName
-    // );
-
-    // $actionName = $router->getActionName() ?? 'index';
-
-    // $dispatcher->setActionName(
-    //     $actionName
-    // );
-
-    // $params = $router->getParams();
-
-    // $dispatcher->setParams(
-    //     $params
-    // );
-
-    // $request = new \Phalcon\Http\Request();
-
-    // $customRouter = new \AltoRouter();
-    // $customRouter = new \Harpya\IP\Lib\Router($di);
-
-    // $customRouter->loadFromFolder($rootPath . '/routes');
-
-    // $match = $customRouter->match();
-
-    // $processViewRender = true;
-
-    // if (is_array($match) && is_callable($match['target'])) {
-    //     $processViewRender = false;
-    //     $statusCode = 200;
-    //     try {
-    //         $resp = call_user_func_array($match['target'], $match['params']);
-    //     } catch (\Exception $ex) {
-    //         $resp = [
-    //             'msg' => $ex->getMessage()
-    //         ];
-    //         $statusCode = $ex->getCode();
-    //     }
-
-    //     if ($resp === Constants::RESPONSE_PROCEED_VIEW_PROCESSING) {
-    //         $processViewRender = true;
-    //     } elseif (is_array($resp)) {
-    //         $resp = \json_encode($resp);
-    //         $response = new Phalcon\Http\Response($resp, $statusCode);
-    //         $response->setContentType('application/json');
-    //     } else {
-    //         $response = new Phalcon\Http\Response($resp, $statusCode);
-    //     }
-    // }
-
-    // $controllerName = $dispatcher->getControllerName() ?? 'index';
-    // $actionName = $dispatcher->getActionName() ?? 'index';
-
-    // if ($processViewRender) {
-    //     $controller = $dispatcher->dispatch();
-
-    //     $controllerName = $dispatcher->getControllerName() ?? 'index';
-    //     $actionName = $dispatcher->getActionName() ?? 'index';
-
-    //     // View
-    //     $view->render(
-    //         $controllerName,
-    //         $actionName,
-    //         $dispatcher->getParams()
-    //     );
-
-    //     // View
-    //     $view->finish();
-
-    //     $response = $controller->response;
-    //     //$dispatcher->getReturnedValue();
-
-    //     // If controller did  return nothing, get the view.
-    //     if (!$response->getContent()) {
-    //         $response->setContent(
-    //             $view->getContent()
-    //         );
-    //     }
-    // }
-    // }
-
     $app->handle(
         $_SERVER['REQUEST_URI']
     );
-
-    // if ($response instanceof ResponseInterface) {
-    //     $response->send();
-    // }
 } catch (\Exception $e) {
     echo $e->getMessage() . '<br>';
     echo '<pre>' . $e->getTraceAsString() . '</pre>';
