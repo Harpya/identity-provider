09de553cd7169097066a9ff99f5c68c3a93302d4
Eduardo Luz <eduluz1976@gmail.com>
2020-11-13 22:34:52 -0800

    Configuring tests.

--- app/bootstrap.tests.php
+++ app/bootstrap.tests.php
@@ -1,14 +1,18 @@
 <?php
 declare(strict_types=1);
 
-// error_reporting(E_ALL);
-
-use Dotenv\Dotenv;
+error_reporting(E_ALL & ~E_NOTICE);
 
 $rootPath = realpath('.');
 require_once $rootPath . '/vendor/autoload.php';
 
+use Dotenv\Dotenv;
+
 /**
  * Load ENV variables
  */
-Dotenv::createImmutable($rootPath, 'tests.conf')->load();
+if (file_exists(($rootPath . 'tests.conf'))) {
+    Dotenv::createImmutable($rootPath, 'tests.conf')->load();
+} else {
+    die("File $rootPath/tests.conf does not exists ");
+}
